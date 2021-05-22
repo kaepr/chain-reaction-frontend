c7a5682113442d5678e01a140536116ecb970331
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-05-22 18:25:57 +0530

    login forms

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,17 +9,15 @@ export const Navbar = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <div>
-      <nav className="bg-gray-800">
+    <div className="">
+      <nav className="bg-gray-900">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
-                <img
-                  className="h-8 w-8"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                  alt="Workflow"
-                />
+                <span className="font-bold text-white text-lg">
+                  <Link to="/">CRO</Link>
+                </span>
               </div>
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
@@ -39,7 +37,7 @@ export const Navbar = () => {
               <button
                 onClick={() => setOpen(!isOpen)}
                 type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
--- src/pages/Auth/Login.js
+++ src/pages/Auth/Login.js
@@ -1,5 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import API from '../../utils/api/api';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <div className="">
+      <div className="flex flex-col justify-center items-center mt-16">
+        <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <h2 className="text-center font-bold text-lg p-2">Login</h2>
+          <div class="mb-4">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="username"
+            >
+              Email
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              placeholder="Email"
+            />
+          </div>
+          <div class="mb-6">
+            <label
+              class="block text-gray-700 text-sm font-bold mb-2"
+              for="password"
+            >
+              Password
+            </label>
+            <input
+              class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              type="password"
+              placeholder="**********"
+            />
+          </div>
+          <div class="flex items-center justify-between">
+            <button
+              class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Login
+            </button>
+            <Link
+              className="inline-block align-baseline font-bold text-sm text-gray-500 hover:text-gray-800"
+              to="/register"
+            >
+              Register
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
