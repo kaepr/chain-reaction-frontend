f9f7c459485bfe7cbed2256d0007c903c2543f7b
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-05-22 17:55:52 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
 import { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import API from './utils/api/api';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import { Login, Register } from './pages/Auth';
 
 function App() {
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const res = await API.post('/api/auth/login', {
-          email: 'test5@gmail.com',
-          password: '123456',
-        });
-        console.log('res = ', res.data);
-      } catch (err) {
-        console.log('err', err);
-      }
-    }
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     try {
+  //       const res = await API.post('/api/auth/login', {
+  //         email: 'test5@gmail.com',
+  //         password: '123456',
+  //       });
+  //       console.log('res = ', res.data);
+  //     } catch (err) {
+  //       console.log('err', err);
+  //     }
+  //   }
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <input type="text"></input>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/NavbarData.js
@@ -0,0 +1,18 @@
+export const LoggedOutRoutes = [
+  {
+    path: '/',
+    name: 'Home',
+  },
+  {
+    path: '/login',
+    name: 'Login',
+  },
+  {
+    path: '/register',
+    name: 'Register',
+  },
+  {
+    path: '/about',
+    name: 'About',
+  },
+];
