b72cc62592f37f1c51d00727203e71027a461a0e
Shagun Agrawal <agrawalshagun07@gmail.com>
2021-05-28 23:52:23 +0530

    fix port

--- src/pages/Play/Socket/socket.js
+++ src/pages/Play/Socket/socket.js
@@ -7,17 +7,20 @@ export function init() {
   let url = 'http://localhost:5001/';
 
   if (process.env.NODE_ENV === 'production') {
-    url = 'https://chain-reaction-backend.herokuapp.com:5001/';
+    url = 'https://chain-reaction-backend.herokuapp.com:/';
   }
 
-  const socket = openSocket('https://chain-reaction-backend.herokuapp.com/', {
-    transports: ['websocket', 'polling'],
-    query: {
-      data: 'hello from client',
-      token: token,
-    },
-    withCredentials: true,
-  });
+  const socket = openSocket(
+    'https://chain-reaction-backend.herokuapp.com:5001/',
+    {
+      transports: ['websocket', 'polling'],
+      query: {
+        data: 'hello from client',
+        token: token,
+      },
+      withCredentials: true,
+    }
+  );
 
   return socket;
 }
